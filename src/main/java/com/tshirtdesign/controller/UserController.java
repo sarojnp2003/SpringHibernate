51ee9426c7374f5e3de87cf2d970454d3a09242e
Saroj Gautam <sarojnp2003@gmail.com>
2016-12-09 18:26:23 +0545

    Hateoas implementation

--- src/main/java/com/tshirtdesign/controller/UserController.java
+++ src/main/java/com/tshirtdesign/controller/UserController.java
@@ -5,17 +5,21 @@ package com.tshirtdesign.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.tshirtdesign.model.User;
+import com.tshirtdesign.resource.UserResource;
 import com.tshirtdesign.service.UserService;
 
 /**
@@ -36,18 +40,15 @@ public class UserController {
 
 	// Register a new user
 	@PostMapping(value = "/register")
-	public ResponseEntity<Void> addUser() {
-		User user = new User();
-		user.setFirstName("Saroj2");
-		user.setLastName("Gautam2");
-		user.setEmail("sarojnp2003+2@gmail.com");
-		user.setPassword("test123");
-		user.setAddress("nepal");
-		user.setTelephone("1234567890");
-		user.setRole("ROLE_ADMIN");
-
-		userService.saveUser(user);
-		return new ResponseEntity<Void>(HttpStatus.CREATED);
+	public ResponseEntity<UserResource> registerUser(@RequestBody UserResource userResource,
+			HttpServletRequest request) {
+		try {
+			userService.saveUser(userResource.toUser());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return new ResponseEntity<UserResource>(userResource,HttpStatus.CREATED);
 	}
 
 	// Get all users
@@ -56,21 +57,21 @@ public class UserController {
 		List<User> user = userService.findAllUser();
 		return new ResponseEntity<List<User>>(user, HttpStatus.OK);
 	}
-	
-	@GetMapping(value="/basic")
-    public ModelAndView visitHome() {
+
+	@GetMapping(value = "/basic")
+	public ModelAndView visitHome() {
 		ModelAndView model = new ModelAndView("basic");
 		model.addObject("title", "Index Page");
 		model.addObject("message", "This is index page");
 		return model;
-    }
-         
-	@GetMapping(value="/admin")
-    public ModelAndView visitAdmin() {
-        ModelAndView model = new ModelAndView("admin");
-        model.addObject("title", "Admministrator Control Panel");
-        model.addObject("message", "This page demonstrates how to use Spring security.");
-         
-        return model;
-    }
+	}
+
+	@GetMapping(value = "/admin")
+	public ModelAndView visitAdmin() {
+		ModelAndView model = new ModelAndView("admin");
+		model.addObject("title", "Admministrator Control Panel");
+		model.addObject("message", "This page demonstrates how to use Spring security.");
+
+		return model;
+	}
 }
---
+++ src/main/java/com/tshirtdesign/resource/UserResource.java
@@ -0,0 +1,146 @@
+/**
+ * 
+ */
+package com.tshirtdesign.resource;
+
+import org.springframework.hateoas.ResourceSupport;
+
+import com.tshirtdesign.model.User;
+
+/**
+ * @author saroj-gautam
+ *
+ */
+public class UserResource extends ResourceSupport {
+
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+
+	private String password;
+
+	private String address;
+
+	private String telephone;
+
+	private String role;
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName
+	 *            the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName
+	 *            the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address
+	 *            the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the telephone
+	 */
+	public String getTelephone() {
+		return telephone;
+	}
+
+	/**
+	 * @param telephone
+	 *            the telephone to set
+	 */
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	/**
+	 * @return the role
+	 */
+	public String getRole() {
+		return role;
+	}
+
+	/**
+	 * @param role
+	 *            the role to set
+	 */
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	public User toUser() {
+		User user = new User();
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setEmail(email);
+		user.setPassword(password);
+		user.setAddress(address);
+		user.setTelephone(telephone);
+		user.setRole(role);
+		return user;
+	}
+}
